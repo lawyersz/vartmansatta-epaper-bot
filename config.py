46c75f03db48921c0d1bd6a00ed64a71d1656a57
lawyersz <alzebra_9@yahoo.co.in>
2026-08-01 16:32:51 +0530

    Add configuration settings for Telegram and WordPress

---
+++ config.py
@@ -0,0 +1,11 @@
+# Telegram Configuration
+TELEGRAM_BOT_TOKEN = ""
+TELEGRAM_CHANNEL_ID = "-1004310998317"
+
+# WordPress Configuration
+WP_URL = "https://vartmansattanews.com"
+WP_USERNAME = ""
+WP_APP_PASSWORD = ""
+
+# Folder where downloaded files will be stored
+DOWNLOAD_FOLDER = "downloads"
